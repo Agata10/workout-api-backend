d6017d38bc31e1a4c5adae6cf1d5ac98a15b5c78
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-11-06 14:52:22 -0700

    Add views for practice

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  //set header content type
+  res.setHeader('Content-type', 'text/html');
 
-  res.write('Server responded');
+  let path = './views/';
+
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      // res.end(data)
+    }
+    res.end();
+  });
 });
 
 server.listen(3000, 'localhost', () => {
